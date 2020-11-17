7b82c968f30aba4a77c1085d546d578a193d7ab1
yun <n856839@gmail.com>
2020-11-17 12:24:37 +0800

    feat: modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,9 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
 // querystring ?
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) ||
-      restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
-  })
-  res.render('index', { restaurant: restaurants, keyword: keyword })
+router.get('/', (req, res) => {
+  const keyword = req.query.keyword.trim().toLocaleLowerCase()
+  Restaurant.find()
+    .lean()
+    .then(restaurants => restaurants.filter(restaurant => {
+      return restaurant.name.toLocaleLowerCase().includes(keyword) ||
+        restaurant.category.toLocaleLowerCase().includes(keyword)
+    }))
+    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(error => console.error(error))
 })
+
+module.exports = router
