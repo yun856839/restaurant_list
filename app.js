5bc49b753c5a209230125502dc10458a4e092360
yun <n856839@gmail.com>
2020-12-28 12:52:11 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -13,6 +14,11 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: "RestaurantSecret",
+  resave: false,
+  saveUninitialized: true
+}))
 // setting static files
 app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
