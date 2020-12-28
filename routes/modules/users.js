e165ddf529e600c9613c64028bff4a9632775957
yun <n856839@gmail.com>
2020-12-28 18:32:32 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,6 +6,7 @@ const passport = require('passport')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
+  res.locals.error_msg = req.flash('error')
   res.render('login')
 })
 
@@ -21,30 +22,50 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '兩次密碼不相符' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已註冊' })
       res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
     }
+
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+
   })
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '已經成功登出')
   res.redirect('/users/login')
 })
 
